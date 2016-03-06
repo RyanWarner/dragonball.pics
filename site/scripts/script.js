29520029032c4b315a04ad7a4744f6b9446533bc
Ryan Warner <warner.ryan@gmail.com>
2016-03-06 08:27:04 -0800

    add page titles

--- site/scripts/script.js
+++ site/scripts/script.js
@@ -1,3 +1,15 @@
 'use strict';
 
-console.log( 'Javascript.' );
+DomReady.ready( function(  )
+{
+	var navElement = document.getElementsByClassName( 'nav' )[ 0 ];
+	var toggleNavButton = document.getElementsByClassName( 'nav-toggle' )[ 0 ];
+
+	var toggleNav = function(  )
+	{
+		navElement.classList.toggle( 'hidden' );
+		toggleNavButton.classList.toggle( 'collapsed' );
+	};
+
+	toggleNavButton.addEventListener( 'click', toggleNav, false );
+} );
